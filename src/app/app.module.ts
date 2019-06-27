191e0387cc1ed8597cfdcd3b76ce042d53af7f4a
idanhershman <idanh@herolo.co.il>
2019-06-27 22:08:50 +0300

    main_weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { WeatherMainViewComponent } from './weather-main-view/weather-main-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WeatherMainViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/weather-main-view/weather-main-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-main-view',
+  templateUrl: './weather-main-view.component.html',
+  styleUrls: ['./weather-main-view.component.css']
+})
+export class WeatherMainViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
