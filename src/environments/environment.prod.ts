ba2805748db7474d6b34fe337f59caea0ad1ad08
idanhershman <idanh@herolo.co.il>
2019-07-22 21:40:23 +0300

    fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=hqqt9CBN6GZG01X5ecACK5CfQXMp4r9B&q=",
 };
